c8a4140d555329074c071c69e9bad2c780543cb2
cpickett <cpickett@64e312b2-a51f-0410-8e61-82d0ca0eb02a>
2006-10-13 04:52:05 +0000

    * Renamed tags to use check-x.y.z convention.

---
+++ check/tests/check_check.h
@@ -0,0 +1,17 @@
+#ifndef CHECK_CHECK_H
+#define CHECK_CHECK_H
+
+int sub_nfailed;
+int sub_ntests;
+
+void setup (void);
+void cleanup (void);
+Suite *make_sub_suite(void);
+Suite *make_sub2_suite(void);
+Suite *make_master_suite(void);
+Suite *make_list_suite(void);
+Suite *make_msg_suite(void);
+Suite *make_log_suite(void);
+
+
+#endif /* CHECK_CHECK_H */
